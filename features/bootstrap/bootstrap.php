e4e063aaa0be67e5852714789fa1bf39767e6686
Marat Latypov <lamarat16@gmail.com>
2020-11-01 12:17:39 +0100

    fresh and clear symfony 5.1

--- features/bootstrap/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (!isset($_SERVER['APP_ENV'])) {
-    $_SERVER['APP_ENV'] = 'test';
-    $_SERVER['APP_SECRET'] = 'foobar';
-}
--- src/AppBundle/Repository/PlaceRepository.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AppBundle\Repository;
-
-class PlaceRepository extends \Doctrine\ORM\EntityRepository
-{
-}
