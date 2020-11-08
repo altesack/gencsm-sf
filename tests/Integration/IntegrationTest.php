e314bc07fe6b6ae93d00ac11ab20636e0c7ee36f
Marat Latypov <lamarat16@gmail.com>
2020-11-08 19:07:11 +0100

    Code style fixes

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -11,30 +11,44 @@ class IntegrationTest extends AbstractIntegrationTest
         $this->assertEquals(404, $response->getStatusCode());
     }
 
+    /**
+     * Trying to get the person #1
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function testTestPersonShouldBe()
     {
-        $response = self::getClient()->get('/api/person/1', ['http_errors' => false]);
+        $response = self::getClient()->get(
+            '/api/person/1',
+            ['http_errors' => false]
+        );
         $this->assertEquals(200, $response->getStatusCode());
 
         $result = json_decode($response->getBody());
         $this->assertEquals(1, $result->id);
-        $this->assertEquals("Veit", $result->name);
-        $this->assertEquals("Bach", $result->surname);
+        $this->assertEquals('Veit', $result->name);
+        $this->assertEquals('Bach', $result->surname);
         $this->assertEquals(1, $result->husbandInFamilies[0]->id);
     }
 
+    /**
+     * Trying to get the family #1
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function testTestFamilyShouldBe()
     {
-        $response = self::getClient()->get('/api/family/1', ['http_errors' => false]);
+        $response = self::getClient()->get(
+            '/api/family/1',
+            ['http_errors' => false]
+        );
         $this->assertEquals(200, $response->getStatusCode());
 
         $result = json_decode($response->getBody());
         $this->assertEquals(1, $result->id);
         $this->assertEquals(1, $result->husband->id);
-        $this->assertEquals("Veit", $result->husband->name);
-        $this->assertEquals("Bach", $result->husband->surname);
+        $this->assertEquals('Veit', $result->husband->name);
+        $this->assertEquals('Bach', $result->husband->surname);
         $this->assertEquals(2, $result->children[0]->id);
-        $this->assertEquals("Johannes Hans", $result->children[0]->name);
-        $this->assertEquals("Bach", $result->children[0]->surname);
+        $this->assertEquals('Johannes Hans', $result->children[0]->name);
+        $this->assertEquals('Bach', $result->children[0]->surname);
     }
 }
