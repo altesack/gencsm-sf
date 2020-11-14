49164975429ede1f20a424189be8a14f11a97707
Marat Latypov <lamarat16@gmail.com>
2020-11-14 15:37:24 +0100

    first tests

---
+++ tests/Factory/FileFactoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Factory\FileFactory;
+use PHPUnit\Framework\TestCase;
+
+class FileFactoryTest extends TestCase
+{
+
+}
---
+++ tests/Factory/PlaceFactoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Factory\PlaceFactory;
+use PHPUnit\Framework\TestCase;
+
+class PlaceFactoryTest extends TestCase
+{
+
+}
